7b5a99dba2fd73cde9adfe3353656a66167c8c12
Timothy Cromwell <nerdster@nerdfamily.com>
2026-04-16 16:37:06 -0700

    Initial git push - Tim

---
+++ include/_gameManager.h
@@ -0,0 +1,16 @@
+#ifndef _GAMEMANAGER_H
+#define _GAMEMANAGER_H
+
+
+class _gameManager
+{
+    public:
+        _gameManager();
+        virtual ~_gameManager();
+
+    protected:
+
+    private:
+};
+
+#endif // _GAMEMANAGER_H
---
+++ src/_gameManager.cpp
@@ -0,0 +1,11 @@
+#include "_gameManager.h"
+
+_gameManager::_gameManager()
+{
+    //ctor
+}
+
+_gameManager::~_gameManager()
+{
+    //dtor
+}
