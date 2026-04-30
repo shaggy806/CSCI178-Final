8a66a672cdadf37c9c2918719c75ac6e3aa62957
Tobiah Ens <brickmantoby@gmail.com>
2026-04-30 16:54:45 -0700

    Fixed kickanim

--- src/gleep.cpp
+++ src/gleep.cpp
@@ -33,8 +33,8 @@ void gleep::initAnimations()
     animations.push_back({20});
     animations.push_back({0});
     animations.push_back({23});
-    animations.push_back({24});
     animations.push_back({25});
+    animations.push_back({24});
 }
 
 void gleep::updatePlayer(float dt)
@@ -45,8 +45,8 @@ void gleep::updatePlayer(float dt)
 
     if (isKicking)
     {
-        if (animation % 2 == 1 || animation == 6) animation = 6;
-        else if (animation % 2 == 0 || animation == 7) animation = 7;
+        if (animation % 2 == 1 || animation == 7) animation = 7;
+        else if (animation % 2 == 0 || animation == 6) animation = 6;
     }
 
     bool onGround = fabs(yV) < 0.1f;
